0bdfe069582a8b4d6a7932695b5a5eb76b087147
lls <ryannotdead@gmail.com>
2019-01-21 16:37:39 +0800

    server

--- server.js
+++ server.js
@@ -20,11 +20,19 @@ var url = require('url');
 function start(route, handle) {
     function onRequest(request, response) {
         var pathname = url.parse(request.url).pathname;
-        
+        // var postData = "";
         if(request.url === '/favicon.ico') return; //处理自动请求
-
         console.log('Request for ' + pathname + ' received.');
-        route(handle, pathname, response);
+        route(handle, pathname, response, request);
+        // request.setEncoding("utf8");
+        // request.addListener("data", function(postDataChunk) {
+        //     postData += postDataChunk;
+        //     console.log("Received POST data chunk '" + postDataChunk + "'.");
+        // });
+        // request.addListener("end", function() {
+        //     route(handle, pathname, response, postData);
+        // });
+        // route(handle, pathname, response);
         // route(pathname);
         // route(handle, pathname);
 
