3d12fd4b3b754b8652d2a6d8e8c9f9fb2c980d01
lls <ryannotdead@gmail.com>
2019-01-21 16:37:49 +0800

    router

--- router.js
+++ router.js
@@ -1,9 +1,9 @@
-function route(handle ,pathname, response) {
+function route(handle ,pathname, response, request) {
     console.log('About to route a request for ' + pathname);
     if (typeof(handle[pathname]) === 'function') {
         // handle[pathname]();
         // return handle[pathname]();
-        handle[pathname](response);
+        handle[pathname](response, request);
     } else {
         console.log("No request handler found for " + pathname);
         // return "404 not found";
